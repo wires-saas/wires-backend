37a156b2c581a3ca720628a55cb2aa99d1bd8512
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-07-14 12:29:16 +0200

    fix(tests): health

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, TerminusModule],
       controllers: [HealthController],
     }).compile();
 
