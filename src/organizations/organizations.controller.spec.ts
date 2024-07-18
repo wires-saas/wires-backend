4219fecbc9a72b37ef12b6f5e340d78412d8c805
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-07-18 19:15:46 +0200

    tests: mongo compliance

--- src/organizations/organizations.controller.spec.ts
+++ src/organizations/organizations.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationsController } from './organizations.controller';
+import { OrganizationsService } from './organizations.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Organization,
+  OrganizationSchema,
+} from './schemas/organization.schema';
 
 describe('OrganizationsController', () => {
   let controller: OrganizationsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forFeature([
+          { name: Organization.name, schema: OrganizationSchema },
+        ]),
+      ],
       controllers: [OrganizationsController],
+      providers: [OrganizationsService],
     }).compile();
 
     controller = module.get<OrganizationsController>(OrganizationsController);
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { EncryptService } from '../commons/encrypt.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+      ],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, EncryptService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
