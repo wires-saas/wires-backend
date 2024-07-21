5380e3cd30b29606eb4cad8004df0813e18c9ab7
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-07-21 18:40:12 +0200

    fix(users): password on update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,11 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, [] as const),
 ) {
+  @IsOptional()
+  @IsString()
   password: string;
 }
