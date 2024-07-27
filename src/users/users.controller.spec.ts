1a15fc60c3be6059202a94fb79cef852ef0fba0d
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-07-28 01:58:52 +0200

    fix(tests): users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,31 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CaslAbilityFactory } from '../rbac/casl/casl-ability.factory';
+import { AuthGuard } from '../auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 const mockUsersService = {
   create: jest.fn(),
   findAll: jest.fn(),
   findOne: jest.fn(),
+  findOneByEmail: jest.fn(),
   update: jest.fn(),
   remove: jest.fn(),
+};
+
+const authGuardMock = {
+  canActivate: jest.fn(),
+};
+
+const mockCaslAbilityFactory = {
+  createForUser: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
 };
 
 describe('UsersController', () => {
   let controller: UsersController;
   let service: typeof mockUsersService;
+  let caslAbilityFactory: typeof mockCaslAbilityFactory;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [{ provide: UsersService, useValue: mockUsersService }],
+      providers: [
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: CaslAbilityFactory, useValue: mockCaslAbilityFactory },
+        { provide: AuthGuard, useValue: authGuardMock },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
     service = module.get<typeof mockUsersService>(UsersService);
+    caslAbilityFactory =
+      module.get<typeof mockCaslAbilityFactory>(CaslAbilityFactory);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+    expect(caslAbilityFactory).toBeDefined();
   });
 });
