523d028cda7d23f2ecbf579b7e16f869362201e9
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-09-14 01:14:12 +0200

    tests(ai): fix ai testsuite

--- src/ai/ai.service.spec.ts
+++ src/ai/ai.service.spec.ts
@@ -1,13 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { AiService } from './ai.service';
+import { TestUtils } from '../shared/utils/test.utils';
+
+const mockAiService = {
+  createGPT: jest.fn(),
+};
 
 describe('AiService', () => {
   let service: AiService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AiService],
-    }).compile();
+    const module: TestingModule = await TestUtils.createTestingModule({
+      providers: [{ provide: AiService, useValue: mockAiService }],
+    });
 
     service = module.get<AiService>(AiService);
   });
--- src/ai/gpt.controller.spec.ts
+++ src/ai/gpt.controller.spec.ts
@@ -1,15 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { GptController } from './gpt.controller';
 import { AiService } from './ai.service';
+import { TestUtils } from '../shared/utils/test.utils';
+import { GptService } from './gpt.service';
+
+const mockAiService = {
+  createGPT: jest.fn(),
+};
+
+const mockGptService = {
+  request: jest.fn(),
+};
 
 describe('GptController', () => {
   let controller: GptController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await TestUtils.createTestingModule({
       controllers: [GptController],
-      providers: [AiService],
-    }).compile();
+      providers: [
+        { provide: AiService, useValue: mockAiService },
+        { provide: GptService, useValue: mockGptService },
+      ],
+    });
 
     controller = module.get<GptController>(GptController);
   });
