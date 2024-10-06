d8c18b853af9d5b708790e73c5cbc9885c49f5e3
Antoine Gautrain <aGautrain@users.noreply.github.com>
2024-10-06 23:13:46 +0200

    fix(blocks): tests

--- src/blocks/blocks.controller.spec.ts
+++ src/blocks/blocks.controller.spec.ts
@@ -1,15 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { BlocksController } from './blocks.controller';
 import { BlocksService } from './blocks.service';
+import { TestUtils } from '../shared/utils/test.utils';
+
+const mockBlocksService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findAllOfOrganization: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+};
 
 describe('BlocksController', () => {
   let controller: BlocksController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await TestUtils.createTestingModule({
       controllers: [BlocksController],
-      providers: [BlocksService],
-    }).compile();
+      providers: [
+        {
+          provide: BlocksService,
+          useValue: mockBlocksService,
+        },
+      ],
+    });
 
     controller = module.get<BlocksController>(BlocksController);
   });
--- src/blocks/blocks.service.spec.ts
+++ src/blocks/blocks.service.spec.ts
@@ -1,13 +1,33 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { TestingModule } from '@nestjs/testing';
 import { BlocksService } from './blocks.service';
+import { TestUtils } from '../shared/utils/test.utils';
+import { Block } from './schemas/block.schema';
+import { getModelToken } from '@nestjs/mongoose';
+
+const mockBlockModel = {
+  save: jest.fn(),
+  find: jest.fn(),
+  findById: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
+  findByIdAndDelete: jest.fn(),
+  insertMany: jest.fn(),
+  deleteMany: jest.fn(),
+  deleteOne: jest.fn(),
+};
 
 describe('BlocksService', () => {
   let service: BlocksService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BlocksService],
-    }).compile();
+    const module: TestingModule = await TestUtils.createTestingModule({
+      providers: [
+        BlocksService,
+        {
+          provide: getModelToken(Block.name),
+          useValue: mockBlockModel,
+        },
+      ],
+    });
 
     service = module.get<BlocksService>(BlocksService);
   });
