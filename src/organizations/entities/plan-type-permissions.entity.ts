55f83de83b2b77bd77ff1b9493e863af0e0c0494
Antoine <antoine.gautrain@gmail.com>
2024-11-25 18:05:03 +0100

    feat(plans): permissions restrictions

--- src/organizations/entities/plan-type-permissions.entity.ts
+++ src/organizations/entities/plan-type-permissions.entity.ts
@@ -29,8 +29,47 @@ export const PlanTypePermissions: Record<
   PlanType,
   Array<{ action: Action; subject: Subject }>
 > = {
-  [PlanType.FREE]: [...noRestrictions],
-  [PlanType.BASIC]: [...noRestrictions],
-  [PlanType.EXTENDED]: [...noRestrictions],
+  [PlanType.FREE]: [
+    { action: Action.Manage, subject: Subject.Organization },
+    { action: Action.Read, subject: Subject.Role },
+
+    { action: Action.Manage, subject: Subject.User },
+    { action: Action.Manage, subject: Subject.UserRole },
+    { action: Action.Manage, subject: Subject.Billing },
+
+    { action: Action.Read, subject: Subject.Feed },
+    { action: Action.Read, subject: Subject.FeedRun },
+    { action: Action.Read, subject: Subject.Article },
+    { action: Action.Read, subject: Subject.Tag },
+
+    { action: Action.Read, subject: Subject.Block },
+    { action: Action.Read, subject: Subject.Folder },
+
+    { action: Action.Read, subject: Subject.Gpt },
+    { action: Action.Read, subject: Subject.GptRequest },
+
+    { action: Action.Read, subject: Subject.ContactsProvider },
+    { action: Action.Read, subject: Subject.EmailsProvider },
+  ],
+  [PlanType.BASIC]: [
+    // Disabling fine-grained permissions, and AI-related permissions
+    ...noRestrictions.map((p) => {
+      if ([Subject.Role, Subject.Gpt, Subject.GptRequest].includes(p.subject)) {
+        return { subject: p.subject, action: Action.Read };
+      } else {
+        return p;
+      }
+    }),
+  ],
+  [PlanType.EXTENDED]: [
+    // Disabling fine-grained permissions
+    ...noRestrictions.map((p) => {
+      if ([Subject.Role].includes(p.subject)) {
+        return { subject: p.subject, action: Action.Read };
+      } else {
+        return p;
+      }
+    }),
+  ],
   [PlanType.CUSTOM]: [...noRestrictions],
 };
